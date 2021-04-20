6fd2b86bbf36c29536e406531119e01c1a98507c
WenqingXie47 <wenqing_xie@126.com>
2021-04-20 20:30:17 +0800

    update router

--- DishMap/src/app/app-routing.module.ts
+++ DishMap/src/app/app-routing.module.ts
@@ -6,14 +6,19 @@ import { RecipeComponent } from './recipe/recipe.component';
 import { WorldmapComponent } from './worldmap/worldmap.component';
 import { FeedbackComponent } from './feedback/feedback.component';
 import { UploadComponent } from './upload/upload.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/worldmap', pathMatch: 'full' },
   { path: 'worldmap', component: WorldmapComponent },
-  { path: 'cookbook/:nation/:id', component: RecipeComponent },
+  { path: 'recipe/:id', component: RecipeComponent },
   { path: 'cookbook/:nation', component: CookbookComponent },
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent},
   { path: 'feedback', component: FeedbackComponent},
   { path: 'upload', component: UploadComponent},
+  
   
 ];
 
--- DishMap/src/app/models/User.ts
+++ DishMap/src/app/models/User.ts
@@ -1,5 +1,6 @@
 export interface User {
     id: number;
+    emailAddress: string;
     username: string;
     password: string;
 
