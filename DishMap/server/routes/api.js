f068977ca275a80134050221b4f930feb2ec1ef1
mkx <mkx4499@163.com>
2021-05-03 14:55:17 +0800

    add multer to support upload pictures

--- DishMap/server/routes/api.js
+++ DishMap/server/routes/api.js
@@ -7,6 +7,8 @@ const router = express.Router();
 const RecipeOperation = require("../../db/RecipeOperations");
 const RecipeModel = require("../../db/RecipeSchema");
 
+const tools = require("../tools.js");
+
 router.use(express.json());
 
 /* GET api listing. */
@@ -26,26 +28,6 @@ const NATIONS = [
   "Mexico",
   "Spain",
 ];
-
-// const RECIPES = [
-//   {
-//     id: 1,
-//     title: "burgerTest",
-//     nationality: "Germany",
-//     ingredients: [{ name: "beef", quantity: "1 kg", treatment: "null" }],
-//     directions: ["fry", "done"],
-//   },
-//   {
-//     id: 2,
-//     title: "hot potTest",
-//     nationality: "China",
-//     ingredients: [
-//       { name: "beef", quantity: "1 kg", treatment: "null" },
-//       { name: "chili", quantity: "200 g", treatment: "null" },
-//     ],
-//     directions: ["boil", "done"],
-//   },
-// ];
 
 router.get("/recipe", (req, res) => {
   findAll(function (err, doc) {
@@ -80,26 +62,24 @@ router.get("/recipe/:id", (req, res) => {
 /**
  * 因为数据结构改变，因此不需要再进行 id 自增
  */
-router.post("/recipe/upload", (req, res) => {
-  console.log(req.body.nationality);
-  // var idObj = RecipeOperation.getIdNum();
-  // if (!idObj) {
-  //   var idNum = 1;
-  // } else {
-  //   var idNum = idObj.id + 1;
-  // }
-  var recipe = {
-    // id: idNum,
-    title: req.body.title,
-    //introduction没存上？
-    introduction: req.body.introduction,
-    nationality: req.body.nationality,
-    ingredients: req.body.ingredients,
-    directions: req.body.directions,
-    imgUrl: req.body.imgUrl,
-    popularity: req.body.popularity,
-  };
-  RecipeOperation.insert(recipe);
+router.post("/recipe/upload", tools.multer().single("file"), (req, res) => {
+  res.send({
+    body: req.body,
+    file: req.file,
+  });
+
+  // var recipe = {
+  //   // id: idNum,
+  //   title: req.body.title,
+  //   //introduction没存上？
+  //   introduction: req.body.introduction,
+  //   nationality: req.body.nationality,
+  //   ingredients: req.body.ingredients,
+  //   directions: req.body.directions,
+  //   imgUrl: req.body.imgUrl,
+  //   popularity: req.body.popularity,
+  // };
+  // RecipeOperation.insert(recipe);
 });
 
 router.post("/recipe/feedback", (req, res) => {});
@@ -113,3 +93,23 @@ router.post("/user/login", (req, res) => {});
 router.post("/user/logout", (req, res) => {});
 
 module.exports = router;
+
+// const RECIPES = [
+//   {
+//     id: 1,
+//     title: "burgerTest",
+//     nationality: "Germany",
+//     ingredients: [{ name: "beef", quantity: "1 kg", treatment: "null" }],
+//     directions: ["fry", "done"],
+//   },
+//   {
+//     id: 2,
+//     title: "hot potTest",
+//     nationality: "China",
+//     ingredients: [
+//       { name: "beef", quantity: "1 kg", treatment: "null" },
+//       { name: "chili", quantity: "200 g", treatment: "null" },
+//     ],
+//     directions: ["boil", "done"],
+//   },
+// ];
