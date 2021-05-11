990cf0c69583f768c480fe859806b2e052e4f451
WenqingXie47 <wenqing_xie@126.com>
2021-05-11 22:16:10 +0800

    update

--- DishMap/src/app/recipe/recipe.component.spec.ts
+++ DishMap/src/app/recipe/recipe.component.spec.ts
@@ -1,10 +1,19 @@
 import { HttpClient, HttpHandler } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs/rx';
+import { of } from 'rxjs';
 
 import { RecipeComponent } from './recipe.component';
 
+const paramMap1  = {
+  get( id : string): string{
+    return '123';
+  }
+}
+
 const fakeActivatedRoute = {
+  paramMap : of(paramMap1),
   snapshot: {
     paramMap : {
       get( id : string): string{
@@ -13,6 +22,7 @@ const fakeActivatedRoute = {
     }
   }
 } as ActivatedRoute;
+
 
 describe('RecipeComponent', () => {
   let component: RecipeComponent;
--- DishMap/src/app/recipe/recipe.component.ts
+++ DishMap/src/app/recipe/recipe.component.ts
@@ -55,7 +55,6 @@ export class RecipeComponent implements OnInit {
         console.log(err);
       },
       
-
     );
       
   }
