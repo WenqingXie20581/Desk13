df15d8f31a8e9eb01bddce2fbfc0a8706392c780
Haolong Li <fr20938@bristol.ac.uk>
2021-05-14 00:30:05 +0800

    update

--- .history/DishMap/src/app/error.interceptor.spec_20210514002657.ts
+++ .history/DishMap/src/app/error.interceptor.spec_20210514002657.ts
@@ -1,13 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { Router } from '@angular/router';
+
 import { ErrorInterceptor } from './error.interceptor';
 
-const routerSpy = jasmine.createSpyObj('Router', ['']);
 describe('ErrorInterceptor', () => {
   beforeEach(() => TestBed.configureTestingModule({
     providers: [
       ErrorInterceptor,
-      {provide: Router, useValue: routerSpy}
       ]
   }));
 
